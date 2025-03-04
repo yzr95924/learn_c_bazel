75168dfa1f7af9e81574f2feb6e6126db8169775
yzr95924 <yzr95924@gmail.com>
2025-03-05 00:04:52 +0800

    UPDATE: add script to support bazel-compile-commands-extractor

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # bazel clean --expunge
 bazel build --config=debug @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
+bazel run @hedron_compile_commands//:refresh_all  -- --config=debug
--- scripts/termux_build.sh
+++ scripts/termux_build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
 bazel build --config=debug --config=termux @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
+bazel run @hedron_compile_commands//:refresh_all  -- --config=debug -- --config=termux
