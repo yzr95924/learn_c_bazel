7ac55557a614258d15bd19d01928d090b4f30c85
yzr95924 <yzr95924@gmail.com>
2025-03-05 00:16:44 +0800

    UPDATE: add config flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # bazel clean --expunge
-bazel build --config=debug @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
-bazel run @hedron_compile_commands//:refresh_all  -- --config=debug
+bazel build --compilation_mode=dbg --config=debug @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
+bazel run @hedron_compile_commands//:refresh_all -- --compilation_mode=dbg -- --config=debug
--- scripts/termux_build.sh
+++ scripts/termux_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-bazel build --config=debug --config=termux @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
-bazel run @hedron_compile_commands//:refresh_all  -- --config=debug -- --config=termux
+bazel build --compilation_mode=dbg --config=debug --config=termux @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
+bazel run @hedron_compile_commands//:refresh_all  -- --compilation_mode=dbg -- --config=debug -- --config=termux
