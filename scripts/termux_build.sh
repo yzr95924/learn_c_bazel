e87b36143d1425a3fc64270e862aba0dfe6057f8
yzr95924 <yzr95924@gmail.com>
2025-03-27 11:58:39 +0800

    UPDATE: fix termux scripts

--- scripts/termux_build.sh
+++ scripts/termux_build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 bazel build --config=debug --config=termux @yzr95924_MQSim_bazel//src/app:MQSim --explain=build.log --subcommands=pretty_print
 
-old_python_path="$(bazel info execution_root)/bazel-out/aarch64-fastbuild/bin/external/hedron_compile_commands~/refresh_all.runfiles/rules_python~~python~python_3_11_aarch64-unknown-linux-gnu/bin/python3"
+old_python_path="$(bazel info execution_root)/bazel-out/aarch64-fastbuild/bin/refresh_compile_commands.runfiles/rules_python~~python~python_3_11_aarch64-unknown-linux-gnu/bin/python3"
 current_python_path="$(which python3)"
 
 if [ ! -f $old_python_path ]; then
-    bazel run @hedron_compile_commands//:refresh_all -- --config=termux
+    bazel run //:refresh_compile_commands -- --config=termux
 fi
 
+set -x
 rm $old_python_path
 ln -s $current_python_path $old_python_path
-bazel run @hedron_compile_commands//:refresh_all -- --config=termux
+set +x
+bazel run //:refresh_compile_commands -- --config=termux
