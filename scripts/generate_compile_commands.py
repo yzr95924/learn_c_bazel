ff51244a55685721fc26f0391b4246a3d69ba916
yzr95924 <yzr95924@gmail.com>
2026-02-25 21:39:53 +0800

    UPDATE: fix compile_commands.py

--- scripts/generate_compile_commands.py
+++ scripts/generate_compile_commands.py
@@ -30,7 +30,7 @@ _G_BAZEL_COMPILE_COMMANDS_TOOL = "bazel-compile-commands"
 _G_JSON_FORMAT_TOOL = "jq"
 _G_BAZEL_BUILD_ARGS = ""
 _G_BAZEL_COMPILE_COMMANDS_ARGS = ""
-_G_BAZEL_EXTERNAL_PATH = "external"
+_G_BAZEL_EXTERNAL_PATH = "./external"
 
 def _run_shell_cmd(cmd: str, cwd: str):
     logging.debug("run cmd in %s: %s", cwd, cmd)
@@ -56,10 +56,20 @@ def _get_current_bazel_workspace() -> str:
     cur_dir = os.getcwd()
     ret_code, ret_val = _run_shell_cmd(cmd, cur_dir)
     if ret_code != 0:
-        logging.error("get bazel workspace failed")
+        logging.error("get bazel info workspace failed")
         return ""
     logging.info("current workspace: %s", ret_val.stdout.strip())
     return ret_val.stdout.strip()
+
+# def _get_current_bazel_output_base() -> str:
+#     cmd = "bazel info output_base"
+#     cur_dir = os.getcwd()
+#     ret_code, ret_val = _run_shell_cmd(cmd, cur_dir)
+#     if ret_code != 0:
+#         logging.error("get bazel info output_base failed")
+#         return ""
+#     logging.info("current output_base: %s", ret_val.stdout.strip())
+#     return ret_val.stdout.strip()
 
 def _check_if_tool_exist(tool_name: str) -> bool:
     if shutil.which(tool_name) is not None:
@@ -104,26 +114,31 @@ def replace_bazel_exec_path(json_path) -> int:
         logging.error("get bazel execution_root failed")
         return -1
     bazel_exec_root_path = ret_val.stdout.strip()
-    final_workspace_path = _get_current_bazel_workspace()
-    # create symlink to link external lib in current workspace
-    # logical_workspace_name = "bazel-" + os.path.basename(
-        # _get_current_bazel_workspace())
-    # os.symlink(os.path.join(logical_workspace_name, _G_BAZEL_EXTERNAL_PATH),
-    #            os.path.join(final_workspace_path, _G_BAZEL_EXTERNAL_PATH))
+    current_workspace_path = _get_current_bazel_workspace()
 
     logging.info("replace %s with %s", bazel_exec_root_path,
-                 final_workspace_path)
+                 current_workspace_path)
     try:
         with open(json_path, "r", encoding="utf-8") as json_file:
             json_data = json_file.read()
             new_json_data = json_data.replace(bazel_exec_root_path,
-                                              final_workspace_path)
+                                              current_workspace_path)
 
         with open(json_path, "w", encoding="utf-8") as json_file:
             json_file.write(new_json_data)
     except Exception as e:
         logging.error("replace file content failed, %s", str(e))
         return -1
+
+    # create symlink to output_base external
+    virtual_external_path = os.path.join(current_workspace_path, _G_BAZEL_EXTERNAL_PATH)
+    if os.path.islink(virtual_external_path):
+        logging.info("%s exist, do not create again", virtual_external_path)
+    else:
+        logical_workspace_name = os.path.join(current_workspace_path, "bazel-" + os.path.basename(
+            _get_current_bazel_workspace()))
+        real_external_path = os.path.join(logical_workspace_name, _G_BAZEL_EXTERNAL_PATH)
+        os.symlink(real_external_path, virtual_external_path)
     return 0
 
 if __name__ == "__main__":
